ac2eb744574c4510e0eec22567988e100e841372
MohammedZakhbat <zakhbat.med@gmail.com>
2026-05-19 10:13:05 +0200

    updatetailwind

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
-function  comprobar (){
+function comprobar() {
+
     let plato = document.getElementById("plato").value;
     let vegano = document.getElementById("vegano").checked;
     let gluten = document.getElementById("gluten").checked;
     let lactosa = document.getElementById("lactosa").checked;
 
-
     let puedeComer = true;
 
-    if(plato === "carne" && vegano){
+    if (plato === "carne" && vegano) {
         puedeComer = false;
     }
-    if(plato === "pasta" && gluten){
+
+    if (plato === "pasta" && gluten) {
         puedeComer = false;
     }
-    if(plato === "risotto" && lactosa){
+
+    if (plato === "risotto" && lactosa) {
         puedeComer = false;
     }
-    
+
     let resultado = document.getElementById("resultado");
-    if(puedeComer){
-        resultado.textContent = "Perfecto, pedido correcto";
-        resultado.className = "mt-4 text-center font-semibold text-green-600";
-    }
-    else {
-        resultado.textContent = "Usted no puede comer este plato.";
-        resultado.className = "mt-4 text-center font-semibold text-red-600";
+
+    if (puedeComer) {
+
+        resultado.textContent = "✅ Perfecto, pedido correcto";
+
+        resultado.className =
+            "text-center font-semibold text-green-600 text-sm";
+
+    } else {
+
+        resultado.textContent =
+            "❌ Usted no puede comer este plato.";
+
+        resultado.className =
+            "text-center font-semibold text-red-600 text-sm";
     }
 }
